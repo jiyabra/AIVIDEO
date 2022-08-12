5cb7a6b09d920512beb124f39041dbe6c6db0b78
jiyabra <86106526+jiyabra@users.noreply.github.com>
2022-08-12 02:59:25 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+video = "";
+
+function preload()
+{
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480 , 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video , 0,0,480,380);
+}
+
+function start()
+{
+    ObjectDetector = ml5.ObjectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Statu : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Modal Loaded")
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
