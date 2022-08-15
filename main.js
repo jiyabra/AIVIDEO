31a138b95bbf3eb5c85b3db658e40fe475b53e69
jiyabra <86106526+jiyabra@users.noreply.github.com>
2022-08-15 03:05:23 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -15,12 +15,39 @@ function setup()
 function draw()
 {
     image(video , 0,0,480,380);
+    if(status != "")
+    {
+        objectDetector.detect(video , gotResult);
+        for(i = 0 ; i < object.length ; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects are : " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%"  , objects[i].x + 15 , objects[i].y + 15);
+            nofill()
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+    }
+}
+video = "";
+objects = [];
+status = "";
+
+function gotResult(error , results)
+{
+      if(error)
+      {
+          console.log(error);
+      }
+      console.log(results);
+      objects = results;
 }
 
 function start()
 {
-    ObjectDetector = ml5.ObjectDetector('cocossd' , modelLoaded);
-    document.getElementById("status").innerHTML = "Statu : Detecting Objects";
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded()
